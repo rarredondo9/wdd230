9ec3a9d2352bb77b65b7fcf4fab9eae2a02503db
rarredondo9 <rebeca.arredondo.9@gmail.com>
2024-01-18 22:04:15 +0000

    HTML and bace css

---
+++ lesson02/design-principles/scripts/getdates.js
@@ -0,0 +1,5 @@
+let currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+let lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = lastModified;
