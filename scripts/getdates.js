420ee9140cca65c4fa83b05496342cbb38ba9e35
rarredondo9 <rebeca.arredondo.9@gmail.com>
2024-01-24 22:38:41 +0000

    menu

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,12 +4,14 @@ document.getElementById("year").innerHTML = currentYear;
 let lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = lastModified;
 
-const menuButton = document.querySelector("#menu-button");
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menu-item");
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.getElementById('menu-button');
+    const menuItems = document.querySelectorAll('.menu-item');
 
-menuButton.addEventListener("click", () => {
-    menuItems.forEach((item) => item.classList.toggle("show"));
-    menuButton.classList.toggle("close");
-    menu.classList.toggle("show");
+    menuButton.addEventListener('click', function () {
+        menuItems.forEach(item => {
+            item.classList.toggle('open');
+        });
+        menuButton.classList.toggle('close');
+    });
 });
