176e3751adf9d57e2d92ebed1bc3efd8da978a95
rarredondo9 <rebeca.arredondo.9@gmail.com>
2024-01-26 00:01:12 +0000

    css

--- lesson03/scripts/getDates.js
+++ lesson03/scripts/getDates.js
@@ -1,5 +1,3 @@
 let currentYear = new Date().getFullYear();
 document.getElementById("year").innerHTML = currentYear;
 
-let lastModified = document.lastModified;
-document.getElementById("lastModified").innerHTML = lastModified;
