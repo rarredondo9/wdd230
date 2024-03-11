091fd4d6a2daa8277f3d89a91a3ea20281c77a98
rarredondo9 <rebeca.arredondo.9@gmail.com>
2024-03-11 21:30:07 +0000

    weather omepage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -29,3 +29,37 @@ if (numVisits !== 0) {
 
 numVisits++;
 localStorage.setItem("visits-ls", numVisits);
+
+
+//weather
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("#weather-description");
+
+const urlWeather = "//api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=fa4eb8c44d47c655a9c714e866491c57&units=imperial";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(urlWeather);
+        if (response.ok) {
+            const data = await response.json();
+            //console.log(data); //testing
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    captionDesc.textContent = `${desc}`;
+}
